18d07d2dd755e0619d145bd9fcbc31c4325c2d26
w4terd4wn <35873455+w4terd4wn@users.noreply.github.com>
2018-05-18 08:53:33 -0600

    Add files via upload

---
+++ Simple_Rotation_Encryptor.py
@@ -0,0 +1,27 @@
+def rotation_cipher():
+    letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    encrypt = raw_input('Enter a string to Encrypt')
+    move = 7
+    for x in encrypt:
+        encypted = letters.index(x) + move
+        encrypt = encrypt.replace(letters[encrypted])
+    return encrypt
+
+def rotation_cipher_decryptor():
+    letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    dencrypt = raw_input('Enter a string to Decrypt')
+    move = 7
+    for x in encrypt:
+        encypted = letters.index(x) - move
+        dencrypt = dencrypt.replace(letters[encrypted])
+    return dencrypt
+    
+print("What would you like to do")
+answer = input('Type: 1 to encrypt || 2 to decrypt')
+if (answer == 1):
+    rotation_cipher()
+elif (answer == 2):
+    rotation_cipher_decryptor()
+else:
+    print('that wasn\'t a option')
+    break
