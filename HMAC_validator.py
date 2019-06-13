ab006e6870e1e3c37c99609641e22f395a3623cb
Stanislaw Malec <35873455+w4terd4wn@users.noreply.github.com>
2019-06-13 09:14:49 -0600

    Update HMAC_validator.py

--- HMAC_validator.py
+++ HMAC_validator.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
-
 import hmac
- 
-def gen_tag(msg,key):
-  hm=hmac.new(key.encode())
-  hm.update(msg.encode())
+
+#Run this funtion to validate HMACs. Key and Message will be taken as input and a hash will be return
+
+def runtime():
+  msg_input=input("Please enter a ciphertext")
+  key_input = input('Please enter HMAC key')
+  t = gen_tag(msg_input,key_input)
+  hm=hmac.new(key_input.encode())
+  hm.update(msg_input.encode())
+  print(msg_input + ':' + t)
   return hm.hexdigest()
-msg_input=input("Please enter a message to validate against HMAC")
-key_input = input('Please enter a HMAC key to validate against')
-t = gen_tag(msg_input,key_input)
-print(msg_input + ':' + t)
+
+#To contribute please add fuctions to be able to parse and add files to read through and return files after creating hash
